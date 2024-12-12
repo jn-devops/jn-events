a0bd76101d595481311a053eac01f0e7dbdb01df
renzoDC15 <renzo.carianga@gmail.com>
2024-12-12 23:39:40 +0800

    add Scoreboard livewire page initial

---
+++ app/Livewire/ScoreBoard.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Competition;
+use App\Models\CompetitionJudge;
+use Livewire\Component;
+
+class ScoreBoard extends Component
+{
+    public $competition;
+    public function mount(Competition $competition)
+    {
+        $this->$competition = $competition;
+    }
+
+    public function render()
+    {
+        return view('livewire.score-board');
+    }
+}
