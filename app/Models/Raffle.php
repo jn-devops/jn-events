78c2469c4bef95194914d31a020f1bde265bdfa7
renzoDC15 <renzo.carianga@gmail.com>
2024-12-14 11:59:57 +0800

    make raffle uuid

--- app/Models/Raffle.php
+++ app/Models/Raffle.php
@@ -3,10 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Raffle extends Model
 {
     protected $fillable = [
         'name',
     ];
+    public $incrementing = false;
+    protected $keyType = 'uuid';
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid(); // Generate UUID if not set
+            }
+        });
+    }
 }
