08890b8bf207ba13965ff103af7db88228a38a31
renzoDC15 <renzo.carianga@gmail.com>
2024-12-16 00:19:13 +0800

    add drop if exist raffle winners table migration

--- database/migrations/2024_12_14_120606_create_raffle_winners_table.php
+++ database/migrations/2024_12_14_120606_create_raffle_winners_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('raffle_winners');
         Schema::create('raffle_winners', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('raffle_id');
