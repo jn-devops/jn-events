78aea9292faa8524bbd0fcdc80c23e6443b48891
renzoDC15 <renzo.carianga@gmail.com>
2024-12-15 15:03:38 +0800

    raffle winner filter and setWinner function

--- app/Livewire/RaffleDraw.php
+++ app/Livewire/RaffleDraw.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Checkin;
 use App\Models\Prizes;
 use App\Models\RafflePrize;
+use App\Models\RaffleWinner;
 use Livewire\Component;
 
 class RaffleDraw extends Component
@@ -15,10 +16,12 @@ class RaffleDraw extends Component
     public $employee_names;
     public $sample;
 
+    public $winner;
     public function mount(){
         $this->prizes = Prizes::all();
         $this->employee_names = [];
     }
+
     public function render()
     {
         return view('livewire.raffle-draw')
@@ -42,14 +45,21 @@ class RaffleDraw extends Component
             $checkins = Checkin::whereHas('employee', function ($query) use ($prize) {
                 $query->whereIn('company', $prize->companies)
                     ->whereIn('unit', $prize->units);
-            })->get();
+            })
+                ->whereNotIn('employee_id',RaffleWinner::where('raffle_prize_id',$prize->id)->where('raffle_id',$prize->raffle->id)->pluck('id'))
+                ->get();
             $this->employee_names = $checkins->pluck('name');
             $this->dispatch('start-draw', $this->employee_names);
 //            dd($this->employee_names,$checkins,$prize->companies,$prize->units);
-
 //            $this->employee_names = ['George', 'Samuel', 'Rey', 'Justin'];
 //            $this->dispatch('start-draw', $this->employee_names);
         }
-
+    }
+    public function setWinner(){
+        RaffleWinner::create([
+            'employee_id' => Checkin::where('name',$this->winner)->first()->employee_id,
+            'raffle_id' => $this->chosen_prize_model->raffle_id,
+            'raffle_prize_id' => $this->chosen_prize_model->id,
+        ]);
     }
 }
